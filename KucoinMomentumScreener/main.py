0a900bfdfe1a7bea5181fee0ccdcf38402f28d5a
Nafisul Haque <100821672+nafisul-haque@users.noreply.github.com>
2022-07-19 15:53:58 +1000

    built some functions
    fetching data
    calculating hv

--- KucoinMomentumScreener/main.py
+++ KucoinMomentumScreener/main.py
@@ -5,25 +5,45 @@ import json
 
 client = Market(url = 'https://api.kucoin.com')
 
-klines = pd.DataFrame(client.get_kline('BTC-USDT', '1min'))
+klines = pd.DataFrame(client.get_kline('MLS-USDT', '1min'))
 tickers = client.get_all_tickers()
 
-#print(klines.head(10))
+period = 12
+days = 365
+t10_closes = pd.DataFrame(klines.head(period)[2])
+print(t10_closes)
+t10_closes_list = klines.head(period)[2].tolist()
 
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
-keys = list(tickers.keys())
-keys_t = tickers
-
+usdt_pairs = []
 i = 0
-t = []
-print(range(len(tickers['ticker'])))
-for i in range(len(tickers['ticker'])):
-    t.append(tickers['ticker'][i]['symbol'])
-    i += 1
+for i in range(len(usdt_pairs['ticker'])):
+    if usdt_pairs['ticker'][i]['symbol'][-4:] == 'USDT':
+        tickers.append(usdt_pairs['ticker'][i]['symbol'])
+        i += 1
+    else:
+        continue
 
-#print(tickers['ticker'][0]['symbol'])
-print(len(t))
+def fetch_data():
+    all_t10_closes = []
+    for i in range(len(usdt_pairs)):
+        2
+
+
+
+def historical_vol():
+    log_returns = []
+    j = 1
+    for i in range(len(t10_closes_list)):
+        if j < 11:
+            log_return = np.log(float(t10_closes_list[-j - 1])) / np.log(float(t10_closes_list[-j]))
+            log_returns.append(log_return)
+            j += 1
+        else:
+            break
+
+    print(np.std(log_returns))
+
+    av = np.sqrt(days) * np.std(log_returns)
+    print(f'{av * 100}%')
+
+
